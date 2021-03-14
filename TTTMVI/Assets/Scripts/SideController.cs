50844d0a2e528f6912916cabb70e9b5e3eb6141b
KeeGooRoomiE <59682188+KeeGooRoomiE@users.noreply.github.com>
2021-03-15 00:20:59 +0700

    Polish update

---
+++ TTTMVI/Assets/Scripts/SideController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SideController : MonoBehaviour
+{
+    [SerializeField] private FieldController field;
+    [SerializeField] private int number;
+    [SerializeField] private Image img;
+    [SerializeField] private Color tc;              //--target colour
+    [SerializeField] private Color sc;              //--source colour
+
+    void Start() {
+        img = gameObject.GetComponent<Image>();
+        img.color = tc;
+    }
+    #region //--show proper colour depending on player side
+    void Update() {
+        if (number == 0) {
+            //if blue move is right now
+            if (field.isXTurn) {
+                img.color = tc;
+            } else {
+                img.color = sc;
+            }
+        } else {
+            //if red move is right now
+            if (field.isXTurn) {
+                img.color = sc;
+            } else {
+                img.color = tc;
+            }
+        }
+    }
+    #endregion
+}
