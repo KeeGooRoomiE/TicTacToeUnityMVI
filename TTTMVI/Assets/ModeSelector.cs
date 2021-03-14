42d700f2ba03d965cb3bce124bc863140796b7a5
KeeGooRoomiE <59682188+KeeGooRoomiE@users.noreply.github.com>
2021-03-14 23:58:40 +0700

    First commit

---
+++ TTTMVI/Assets/ModeSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModeSelector : MonoBehaviour
+{
+    [SerializeField] private FieldController field;
+
+    //close element
+    void Update()
+    {
+        if (field.modeSelected) {
+            gameObject.SetActive(false);
+        }
+    }
+
+    //load state
+    void Awake() {
+        field.LoadState();
+    }
+}
